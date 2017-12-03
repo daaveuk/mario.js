f983dde0016ee4599f9bfbd9f6ecaa705219f4ed
David Henderson <me@daave.co.uk>
2017-12-03 15:40:44 +0000

    no idea

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,6 @@ const canvas    = document.getElementById('screen')
 const context   = canvas.getContext('2d')
 
 
-<<<<<<< HEAD
 Promise.all([
     createMario(),
     loadLevel('1-1')
@@ -42,45 +41,4 @@ Promise.all([
             mario.vel.y += gravity * deltaTime;
     }
     timer.start()
-=======
-
-
-
-    
-    Promise.all([
-        createMario(),
-        loadBackgroundSprites(),
-        loadLevel('1-1')
-    ])
-    .then(( [mario, sprites, level]) => {
-        const comp = new Compositor()
-    
-        const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites)
-        comp.layers.push(backgroundLayer)
-        
-        const gravity = 2000
-        mario.pos.set(64, 180)
-        mario.vel.set(200, -600)
-        
-        const spriteLayer = createSpriteLayer(mario)
-        comp.layers.push(spriteLayer)
-    
-    
-    
-        const timer = new Timer(1/60)
-    
-    
-    
-        timer.update = function update(deltaTime) {
-                mario.update(deltaTime)
-                comp.draw(context)
-                mario.vel.y += gravity * deltaTime
-    
-        }
-        
-        timer.start()
-    })
-
-
->>>>>>> 00221b5d478d4b9ab8f9fb4c8a7d59c66954df48
-
+})
